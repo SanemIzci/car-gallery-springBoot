f70ba8cc245b0da0b256c976b24a5848ba66bc17
SanemIzci <sanemizci33@gmail.com>
2025-12-03 16:09:06 +0300

    feat: configure spring security and authentication providers

--- carGallery/src/main/java/com/sanemizci/starter/config/AppConfig.java
+++ carGallery/src/main/java/com/sanemizci/starter/config/AppConfig.java
@@ -1,4 +1,58 @@
 package com.sanemizci.starter.config;
+import com.sanemizci.starter.Model.User;
+import com.sanemizci.starter.Repository.UserRepository;
+import com.sanemizci.starter.exception.BaseException;
+import com.sanemizci.starter.exception.ErrorMessage;
+import com.sanemizci.starter.exception.MessageType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Optional;
+
+@Configuration
 public class AppConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            Optional<User> optional = userRepository.findByUsername(username);
+            if (optional.isEmpty()) {
+                throw new BaseException(new ErrorMessage(MessageType.USERNAME_NOT_FOUND, username));
+            }
+            return optional.get();
+        };
+    }
+    
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();//Auth işlemini yapar
+        authProvider.setUserDetailsService(userDetailsService());//DB'den kullancıyı nasıl bulacağımızı söyler
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder());//şifrelerin nasıl karşılanacağı söylenir
+        return authProvider;
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
--- carGallery/src/main/java/com/sanemizci/starter/config/SecurityConfig.java
+++ carGallery/src/main/java/com/sanemizci/starter/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.sanemizci.starter.config;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    public static final String REGISTER="/register";
+    public static final String AUTHENTICATE="/authenticate";
+    public static final String REFRESH_TOKEN="/refresh_token";
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())//csrf korumasını devre dışı bırakır
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(REGISTER, AUTHENTICATE, REFRESH_TOKEN).permitAll()
+                .anyRequest().authenticated())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+        
+    }
+
+
 }
