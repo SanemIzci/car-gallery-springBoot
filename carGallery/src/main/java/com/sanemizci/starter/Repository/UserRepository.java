c57c28c2b61b4bc313d4ccb4f3a4ae05e3491220
SanemIzci <sanemizci33@gmail.com>
2025-12-03 20:44:00 +0300

    feat: create UserRepository interface for user data access with custom query method

---
+++ carGallery/src/main/java/com/sanemizci/starter/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sanemizci.starter.Repository;
+
+import com.sanemizci.starter.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
