efbe1a2103e736d70d8bf31b03c9c9167c384f1b
SanemIzci <sanemizci33@gmail.com>
2025-12-03 20:39:52 +0300

    feat:dtoBase strucure added

--- carGallery/src/main/java/com/sanemizci/starter/Dto/DtoBase.java
+++ carGallery/src/main/java/com/sanemizci/starter/Dto/DtoBase.java
@@ -1,4 +1,13 @@
 package com.sanemizci.starter.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter@Setter
+
 public class DtoBase {
+    private Long id;
+    private Date createTime;
 }
