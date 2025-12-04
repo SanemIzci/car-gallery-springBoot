882cd15f6a072e735b3a42810a183752d7745704
SanemIzci <sanemizci33@gmail.com>
2025-12-04 19:32:09 +0300

    feat: extend MessageType enum with additional error messages for authentication failures

--- carGallery/src/main/java/com/sanemizci/starter/exception/MessageType.java
+++ carGallery/src/main/java/com/sanemizci/starter/exception/MessageType.java
@@ -9,7 +9,10 @@ public enum MessageType {
     NO_RECORD_EXISTS("1004","No record found"),
     GENERAL_EXCEPTION("9999","General error occured"),
     TOKEN_EXPIRE("1005","Token is expired log in again"),
-    USERNAME_NOT_FOUND("1006","Username not found");
+    USERNAME_NOT_FOUND("1006","Username not found"),
+    USERNAME_OR_PASSWORD_IS_NOT_VALID("1007","Username or password is not valid"),
+    REFRESH_TOKEN_NOT_FOUND("1008","Refresh token not found"),
+    REFRESH_TOKEN_EXPIRED("1009","Refresh token expired");
 
 
     private String code;
