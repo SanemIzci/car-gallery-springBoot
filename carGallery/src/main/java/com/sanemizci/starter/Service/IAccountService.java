1bbc405ee2a92ae4adf07085cbc800e092b84898
SanemIzci <sanemizci33@gmail.com>
2025-12-05 14:36:41 +0300

    feat: add deleteAccount method to IAccountService for account management

--- carGallery/src/main/java/com/sanemizci/starter/Service/IAccountService.java
+++ carGallery/src/main/java/com/sanemizci/starter/Service/IAccountService.java
@@ -6,4 +6,5 @@ import com.sanemizci.starter.Dto.DtoAccountIU;
 public interface IAccountService {
 
     public DtoAccount saveAccount(DtoAccountIU dtoAccountIU);
+    public DtoAccount deleteAccount(Long id);
 }
